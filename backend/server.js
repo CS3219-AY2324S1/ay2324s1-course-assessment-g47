353d243e4ff3bac3d2af7659785abfa6bd29a3c6
shaniceng <ngshanice1@yahoo.com.sg>
2023-09-10 17:52:28 +0800

    Solve merge conflict

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,31 @@
-require('dotenv').config() //Load env variables
+require("dotenv").config(); //Load env variables
 
-const express = require('express')
-const mongoose = require('mongoose')
-const questionRoutes = require('./routes/questions')
+const express = require("express");
+const mongoose = require("mongoose");
+const questionRoutes = require("./routes/questions");
 
-const app = express() //Start up express app
+const app = express(); //Start up express app
 
 //middleware
-app.use(express.json()) //Allows us to use json in the body of the request
-app.use((req,res,next)=> {
-    console.log(req.path,  req.method)
-    next()
-})
+app.use(express.json()); //Allows us to use json in the body of the request
+app.use((req, res, next) => {
+	console.log(req.path, req.method);
+	next();
+});
 
 //routes
-app.use('/api/questions', questionRoutes) //Grabs all the routes from the questions.js file
+app.use("/api/questions", questionRoutes); //Grabs all the routes from the questions.js file
 //The routes will only be used if the path starts with /api/questions
 
 // connect to mongodb
-mongoose.connect(process.env.MONGO_URI)
-    .then(()=> {
-        // only listen for requests when we are connected to the database
-        app.listen(process.env.PORT, (req,res) => {
-            console.log('Listining on port', process.env.PORT)
-        })
-    })
-    .catch((error)=> {
-        console.log(error)
-    })
+mongoose
+	.connect(process.env.MONGO_URI)
+	.then(() => {
+		// only listen for requests when we are connected to the database
+		app.listen(process.env.PORT, (req, res) => {
+			console.log("Listining on port", process.env.PORT);
+		});
+	})
+	.catch((error) => {
+		console.log(error);
+	});
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,62 +1,85 @@
-import { useEffect,  useState } from 'react'
-import { useQuestionsContext } from '../hooks/useQuestionsContext'
+import { useEffect, useState } from "react";
+import { useQuestionsContext } from "../hooks/useQuestionsContext";
+import { Link } from "react-router-dom";
+import "./Home.css";
 
 // components
-import QuestionDetails from '../components/QuestionDetails'
-import QuestionForm from '../components/QuestionForm'
-import QuestionInfo from '../components/QuestionInfo'; // Import the QuestionInfo component
+import QuestionDetails from "../components/QuestionDetails";
+import QuestionForm from "../components/QuestionForm";
+import QuestionInfo from "../components/QuestionInfo"; // Import the QuestionInfo component
+import LoginPage from "../components/Login";
 
+const Home = ({ user, handleLogin, handleLogout }) => {
+	const { questions, dispatch } = useQuestionsContext();
+	const [selectedQuestion, setSelectedQuestion] = useState(null); // State to store the selected question
 
-const Home = () => {
-    const {questions, dispatch} = useQuestionsContext()
-    const [selectedQuestion, setSelectedQuestion] = useState(null); // State to store the selected question
+	// fetch questions from the backend
+	useEffect(() => {
+		const fetchQuestions = async () => {
+			const response = await fetch("/api/questions");
+			const json = await response.json();
 
-    // fetch questions from the backend
-    useEffect(() => {
-        const fetchQuestions = async () => {
-            const response = await fetch('/api/questions') 
-            const json = await response.json()
+			if (response.ok) {
+				dispatch({ type: "SET_QUESTIONS", payload: json });
+			}
+		};
 
-            if(response.ok){
-                dispatch({type: 'SET_QUESTIONS', payload: json})
-            }
-        }
+		fetchQuestions();
+	}, [dispatch]);
 
-        fetchQuestions()
-    }, [dispatch])
-    return (
-      <div className="home">
-        <QuestionForm />
-        <div className="table-container">
-          <table className="table-header">
-            <thead>
-              <tr>
-                <th>Title</th>
-                <th>Complexity</th>
-                <th>Category</th>
-                <th>Created</th>
-                <th>Action</th>
-              </tr>
-            </thead>
-            <tbody>
-              {questions &&
-                questions.map((question) => (
-                  <QuestionDetails
-                    key={question._id}
-                    question={question}
-                    onClick={(question) => {
-                      setSelectedQuestion(question);
-                    }}
-                  />
-                ))}
-            </tbody>
-          </table>
-        </div>
-        <div className="QuestionInfo">
-          <QuestionInfo selectedQuestion={selectedQuestion} />
-        </div>
-      </div>
-    )    
-}
+	return user ? (
+		<>
+			<div className="header">
+				<div className="left">
+					<p>
+						<Link to="/profile">Profile</Link>
+					</p>
+				</div>
+				<div className="center">
+					<h1>Welcome, {user.username}</h1>
+				</div>
+				<div className="right">
+					<p>
+						<button onClick={() => handleLogout()}>Logout</button>
+					</p>
+				</div>
+			</div>{" "}
+			(
+			<div className="home">
+				<QuestionForm />
+				<div className="table-container">
+					<table className="table-header">
+						<thead>
+							<tr>
+								<th>Title</th>
+								<th>Complexity</th>
+								<th>Category</th>
+								<th>Created</th>
+								<th>Action</th>
+							</tr>
+						</thead>
+						<tbody>
+							{questions &&
+								questions.map((question) => (
+									<QuestionDetails
+										key={question._id}
+										question={question}
+										onClick={(question) => {
+											setSelectedQuestion(question);
+										}}
+									/>
+								))}
+						</tbody>
+					</table>
+				</div>
+				<div className="QuestionInfo">
+					<QuestionInfo selectedQuestion={selectedQuestion} />
+				</div>
+			</div>
+		</>
+	) : (
+		<LoginPage onSuccessLogin={handleLogin} />
+	);
+};
 
-export default Home
+export default Home;
