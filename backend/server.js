8f6aa78da76b572d11af15d0771feb733504cab8
shaniceng <ngshanice1@yahoo.com.sg>
2023-09-25 17:13:16 +0800

    Update to include mongodb and postgres data to github

--- backend/server.js
+++ backend/server.js
@@ -17,15 +17,39 @@ app.use((req, res, next) => {
 app.use("/api/questions", questionRoutes);
 
 // Connect to mongodb
-mongoose
-	.connect("mongodb://mongodb:27017/test")
-	// .connect(process.env.MONGO_URI)
-	.then(() => {
-		// only listen for requests when we are connected to the database
-		app.listen(process.env.PORT, (req, res) => {
-			console.log("Listining on port", process.env.PORT);
+// mongoose
+// 	// .connect("mongodb://mongodb:27017/test")
+// 	.connect(process.env.MONGO_URL)
+// 	.then(() => {
+// 		// only listen for requests when we are connected to the database
+// 		app.listen(process.env.PORT, (req, res) => {
+// 			console.log("Listining on port", process.env.PORT);
+// 		});
+// 	})
+// 	.catch((error) => {
+// 		console.log("MongoDB connection error:", error);
+// 	});
+
+const connectWithRetry = () => {
+	mongoose
+		.connect(process.env.MONGO_URL, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			serverSelectionTimeoutMS: 5000, // Timeout in milliseconds for server selection
+		})
+		.then(() => {
+			console.log("Connected to MongoDB");
+			// Only start the application server once the database connection is established
+			app.listen(process.env.PORT, () => {
+				console.log("Listening on port", process.env.PORT);
+			});
+		})
+		.catch((error) => {
+			console.log("MongoDB connection error:", error);
+			// Retry the connection after a delay (e.g., 5 seconds)
+			setTimeout(connectWithRetry, 5000);
 		});
-	})
-	.catch((error) => {
-		console.log("MongoDB connection error:", error);
-	});
+};
+
+// Call the connection function to initiate the connection process
+connectWithRetry();
