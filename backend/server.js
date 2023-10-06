947f45cb06e41b382fe955a6594eb5a93d7ad287
tanxuanyun <tanxuanyun42@gmail.com>
2023-10-06 17:09:50 +0800

    Add test for get question API

--- backend/server.js
+++ backend/server.js
@@ -16,15 +16,33 @@ app.use((req, res, next) => {
 //routes
 app.use("/api/questions", questionRoutes);
 
-// Connect to mongodb
-mongoose
-	.connect(process.env.MONGO_URI)
-	.then(() => {
-		// only listen for requests when we are connected to the database
-		app.listen(process.env.PORT, (req, res) => {
-			console.log("Listining on port", process.env.PORT);
+// Define an async function to handle database connection and start the server
+async function startServer() {
+	try {
+		if (process.env.NODE_ENV === 'test') {
+		console.log('Using Mock DB!');
+		const { MongoMemoryServer } = require('mongodb-memory-server');
+		const mongoServer = await MongoMemoryServer.create();
+		await mongoose.connect(mongoServer.getUri(), { dbName: "test-db" });
+		} else if (process.env.NODE_ENV === 'ci') {
+		console.log('Using MongoDB with GitHub Actions!');
+		await mongoose.connect('mongodb://127.0.0.1:27017/test-db');
+		} else {
+		const con = await mongoose.connect(process.env.MONGO_URI);
+		console.log(`MongoDB Connected: ${con.connection.host}`);
+		}
+
+		// Start the Express app after successful connection
+		app.listen(process.env.PORT, () => {
+		console.log("Listening on port", process.env.PORT);
 		});
-	})
-	.catch((error) => {
-		console.log(error);
-	});
+	} catch (error) {
+	  console.error(error);
+	  process.exit(1);
+	}
+  }
+  
+  // Call the async function to start the server
+  startServer();
+
+module.exports = app;
