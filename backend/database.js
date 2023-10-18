668fbbf6d72d0e0e9b2aebb80aa3a88605424f28
shaniceng <ngshanice1@yahoo.com.sg>
2023-10-18 15:57:23 +0800

    Update assignment2 to automate creating of postgres

--- backend/database.js
+++ backend/database.js
@@ -11,6 +11,37 @@ const pool = new Pool({
 	database: process.env.DB_DATABASE, //Comment out when creating a new database
 });
 
+// // CREATE NEW DATABASE IF IT DOESN'T EXIST
+// const createDatabaseQuery = `
+//   CREATE DATABASE ${process.env.DB_DATABASE};
+// `;
+
+// pool.query(createDatabaseQuery)
+// 	.then((res) => {
+// 		console.log("Database created or already exists");
+// 	})
+// 	.catch((err) => {
+// 		console.error("Error creating database:", err);
+// 	});
+
+// CREATE TABLE IF NOT EXISTS
+const createTableQuery = `
+  CREATE TABLE IF NOT EXISTS accounts (
+    user_id serial PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+  );
+`;
+
+pool.query(createTableQuery)
+	.then((res) => {
+		console.log("Table created or already exists");
+	})
+	.catch((err) => {
+		console.error("Error creating table:", err);
+	});
+
 // // CREATE NEW DATABASE
 // pool.query("CREATE DATABASE cs3219_g47;")
 // 	.then((res) => {
