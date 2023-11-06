880caace13e75bccdeb4fb55be860a625374c88b
tanxuanyun <tanxuanyun42@gmail.com>
2023-11-06 23:28:02 +0800

    Update postgres table

--- backend/database.js
+++ backend/database.js
@@ -30,7 +30,9 @@ const createTableQuery = `
     user_id serial PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL,
+	account_type VARCHAR ( 255 ) NOT NULL,
+    authentication_stats BOOLEAN DEFAULT false
   );
 `;
 
